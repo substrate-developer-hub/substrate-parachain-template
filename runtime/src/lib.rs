cd0b2db47f271ac8c8292752aa55d051d8c9440a
Dan Shields <nukemandan@protonmail.com>
2021-08-19 22:01:29 -0600

    disable teleporting

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -389,7 +389,7 @@ impl Config for XcmConfig {
 	type AssetTransactor = LocalAssetTransactor;
 	type OriginConverter = XcmOriginToTransactDispatchOrigin;
 	type IsReserve = NativeAsset;
-	type IsTeleporter = NativeAsset; // <- should be enough to allow teleportation of UNIT
+	type IsTeleporter = (); // Teleporting is disabled.
 	type LocationInverter = LocationInverter<Ancestry>;
 	type Barrier = Barrier;
 	type Weigher = FixedWeightBounds<UnitWeightCost, Call>;
