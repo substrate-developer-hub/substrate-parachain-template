5dad48a194bcbc787821cf5827e1c2958a0033c6
99kies <1290017556@qq.com>
2022-09-01 17:58:58 +0800

    update

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -66,6 +66,7 @@ pub fn development_config() -> ChainSpec {
 	let mut properties = sc_chain_spec::Properties::new();
 	properties.insert("tokenSymbol".into(), "UNIT".into());
 	properties.insert("tokenDecimals".into(), 12.into());
+	properties.insert("ss58Format".into(), 42.into());
 
 	ChainSpec::from_genesis(
 		// Name
